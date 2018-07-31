5cbb3714210e55ea4cab42ed445f152e8f5f7501
Rhett Jacobs <rhett.jacobs@silverune.com>
2018-07-31 00:12:30 -0700

    Additional test code

--- code/tests.asm
+++ code/tests.asm
@@ -72,3 +72,91 @@ TEST3: *=* "Test 3"
 	rtn			// Return to 6502 mode.
 	break()
 	rts
+
+TEST4: *=* "Test 4"
+	jsr SW16
+    set 1 : $7000
+    set 2 : $7002
+    set 3 : 10
+!LOOP:
+    ldi 1
+    sti 2
+    dcr 3
+    bnz !LOOP-
+    rtn
+
+TEST5: *=* "Test 5"
+	jsr SW16
+
+
+    SET 1 : $1234
+/*    LD R1
+    ST R2
+    LD @R1
+    ST @R2
+    LDD @R3
+    STD @R4
+    POP @R5
+    STP @R6
+    ADD R7
+;    SUB R8
+    POPD @R9
+    CPR R1
+    INR R2
+    DCR R3
+    RTN
+HERE:
+     BR LOOP
+     BNC LOOP
+     BC LOOP
+     BP LOOP
+     BM LOOP
+     BZ LOOP
+     BNZ LOOP
+     BM1 LOOP
+     BNM1 LOOP
+     BK
+     RS
+    BS LOOP
+*/	
+/*    SET R1,$7000
+    SET R2,$7002
+    SET R3,10
+LOOP:
+    LD @R1
+    ST @R2
+    DCR R3
+    BNZ LOOP
+    RTN
+
+; Examples of opcodes (code is not meaningful).
+
+    SET R1,$1234
+    LD R1
+    ST R2
+    LD @R1
+    ST @R2
+    LDD @R3
+    STD @R4
+    POP @R5
+    STP @R6
+    ADD R7
+;    SUB R8
+    POPD @R9
+    CPR R1
+    INR R2
+    DCR R3
+    RTN
+HERE:
+     BR LOOP
+     BNC LOOP
+     BC LOOP
+     BP LOOP
+     BM LOOP
+     BZ LOOP
+     BNZ LOOP
+     BM1 LOOP
+     BNM1 LOOP
+     BK
+     RS
+     BS LOOP	*/
