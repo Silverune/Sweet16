e7f824ecee4e643794450248434fa90786757c80
Silverune <rhett.jacobs@silverune.com>
2018-09-02 22:28:25 +1000

    Cleanup of the memory routines

--- code/sweet16_functions.asm
+++ code/sweet16_functions.asm
@@ -39,20 +39,7 @@
 	.return finalAddress
 }
 
-.function calc_effective_address_negpos(d, currentAddress) {
-	.errorif d > 127, "Displacement too far forward " + d
-	.errorif d < -128, "Displacement too far backward " + d
-
-	.var dval = 0
-/*	.if (d < 0) {
-		.eval dval = $80
-	} else {
-		.eval dval = $7f
-	}
-*/
-	.return calc_effective_address(dval, currentAddress)
-}
-	
+// NOTE - this directly tests documentation I found on SWEET16 in the BR instruction, however, using Kick I do not need to use the calc_effective_address function as can simply calculate it as shown in the "effective_address" implementation below 
 .function test_calculate_effective_address(currentAddress) {
 	.var values = List().add($80, $81, $ff, $00, $01, $7e, $7f)
 	.for (var i = 0	; i < values.size(); i++) {
@@ -60,42 +47,11 @@
 	}
 }
 	
-.function effective_address_new(ea, currentAddress) {
-	.print "ea: " + toHexString(ea.getValue())
-	.print "d: " + (ea.getValue() - currentAddress) + " (" + toHexString(ea.getValue() - currentAddress) + ")"
-	.print "*: " + toHexString(currentAddress)
-	.var result = calc_effective_address_negpos(ea.getValue() - currentAddress, currentAddress)
-	.print "result: " + toHexString(result)
-	.return result
-}
-
 .function effective_address(ea, currentAddress) {
 
 	.var d = (ea.getValue() - currentAddress)
-	.errorif d > 127, "Displacement too far forward " + d
-	.errorif d < -128, "Displacement too far backward " + d
-
-	.if (ea.getType() == AT_ABSOLUTE)
-	{
-		.var relativeAddress = <(ea.getValue() - currentAddress - 2) // account for byte offset
-#if DEBUG
-		.print "ea: $" + toHexString(ea.getValue()) + " currentAddress: $" + toHexString(currentAddress)
-		.print "Relative address: $" + toHexString(relativeAddress)
-#endif
-		.return relativeAddress
-	}
-
-	.if (ea.getType()==AT_IMMEDIATE) {
-#if DEBUG
-		.print "Immediate: #" + ea.getValue
-#endif
-		.return ea.getValue()
-	}
-
-	.var value = CmdArgument(ea.getType(),ea.getValue()+1).getValue()
-#if DEBUG
-	.print "Type: " + ea.getType() + " Value: $"+ toHexString(value)
-#endif	
-	.return value
+	.errorif d > 127, "Displacement too far forward " + d + ".  Move the branch within 127 bytes"
+	.errorif d < -128, "Displacement too far backward " + d + ".  Move the branch within 127 bytes"
+	.var relativeAddress = <(ea.getValue() - currentAddress - 2) // account for byte offset
+	.return relativeAddress
 }
-	
